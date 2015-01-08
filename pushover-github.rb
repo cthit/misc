c806b1d3399c13928fb5e11be14e6b1ecd24dff3
Emil Edholm <bobby@verypublic.org>
2015-01-08 23:42:25 +0100

    Validations and better command line parsing

--- pushover-github.rb
+++ pushover-github.rb
@@ -1,31 +1,48 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 #
-
+require 'optparse'
 require "net/https"
 require 'rubygems'
 require 'json'
 
-unless ARGV[2] && ARGV[3]
-  puts 'You need to supply TOKEN (api-key) as third argument and user key as fourth argument'
-  exit 1
-end
-
-TOKEN = ARGV[2]
-USER  = ARGV[3]
 MAX_URL = 512
 MAX_URL_TITLE = 100
 MAX_TITLE = 250
 MAX_BODY = 1024
 ELLIPSIS = '…'
 
-def allowed_type?(type)
-  # TODO: issue_comment, member, pull_request, repository and team_add
-  valid_types = ['push', 'issues'].freeze
-  unless valid_types.include? type
-    puts type + ' is not a supported type. Supports ' + valid_types.to_s
-    exit 1
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "usage: pushover-github.rb <github-json> -e event -t api-token -u user-key"
+  opts.on('-tTOKEN', '--token=TOKEN', "Application API-token") {|o| options[:token] = o}
+  opts.on('-uUSER', '--user=USER', "User/group key") {|u| options[:user] = u }
+  opts.on('-eEVENT', '--event=EVENT', "The event (push, issues, etc)") {|o| options[:event] = o}
+end.parse!
+
+def valid_options? options
+  failed = false
+  unless options[:token]
+    puts 'No API-token supplied (-t)'
+    failed = true
   end
+  unless options[:user]
+    puts 'No user supplied (-u)'
+    failed = true
+  end
+  unless options[:event]
+    puts 'No event supplied (-e)'
+    failed = true
+  else
+    # TODO: issue_comment, member, pull_request, repository and team_add
+    valid_events = ['push', 'issues'].freeze
+    unless valid_events.include? options[:event]
+      puts options[:event] + ' is not a supported event. Supports ' + valid_events.to_s
+      failed = true
+    end
+  end
+
+  exit 1 if failed
 end
 
 def truncate s, length = MAX_URL_TITLE, ellipsis = ELLIPSIS
@@ -36,14 +53,21 @@ def truncate s, length = MAX_URL_TITLE, ellipsis = ELLIPSIS
   end
 end
 
+def valid_api_keys? token, user
+  if token.size != 30 || user.size != 30
+    abort 'Token and/or user size is wrong. They should be 30 chars'
+  end
+
+  abort 'Wrong token format' unless token.match(/^([A-Za-z0-9]+|)$/)
+  abort 'Wrong user format' unless user.match(/^([A-Za-z0-9]+|)$/)
+end
 
 def parse_issues(payload)
   issue = payload['issue']
   action = payload['action']
   ignore = ['unassigned', 'labeled', 'unlabeled']
   if ignore.include? action
-    puts "Issue parsing: ignoring action '#{action}'"
-    exit 1
+    abort "Issue parsing: ignoring action '#{action}'"
   end
 
   repo = payload['repository']['full_name']
@@ -84,31 +108,23 @@ def parse_push(p)
   {url: url, title: title, message: message, url_title: url_title}
 end
 
-unless ARGV[0]
-  puts 'No GitHub-json supplied as first argument'
-  exit 1
-end
-unless ARGV[1]
-  puts 'No type supplied'
-  exit 1
+abort "No GitHub-JSON supplied" unless ARGV[0]
+
+valid_options? options
+valid_api_keys? options[:token], options[:user]
+begin
+  payload = JSON.parse(ARGV[0])
+rescue JSON::ParserError
+  abort 'Invalid JSON supplied'
 end
 
-
-payload = JSON.parse(ARGV[0])
-type = ARGV[1]
-allowed_type? type
-
-data = send "parse_#{type}", payload
-if data[:url].size > MAX_URL
-  puts "URL too long. Maximum 512 chars. Size: #{url.size}"
-  exit 1
-end
+data = send "parse_#{options[:event]}", payload
 
 push_url = URI.parse("https://api.pushover.net/1/messages.json")
 req = Net::HTTP::Post.new(push_url.path)
 req.set_form_data({
-   :token => TOKEN,
-   :user => USER,
+   :token => options[:token],
+   :user => options[:user],
    :title => (truncate data[:title], MAX_TITLE),
    :message => (truncate data[:message], MAX_BODY),
    :url => data[:url],
@@ -118,9 +134,4 @@ req.set_form_data({
 res = Net::HTTP.new(push_url.host, push_url.port)
 res.use_ssl = true
 res.verify_mode = OpenSSL::SSL::VERIFY_PEER
-res.start {|http| http.request(req) }
-
-puts data[:title]
-puts data[:message]
-puts data[:url_title]
-puts data[:url]
+res.start {|http| puts http.request(req).body }
