9f10f0b1c8c7e4785d2b42d6e0a266157b42ca49
Johan 'Horv' Andersson <drhorv@gmail.com>
2015-09-14 23:17:41 +0200

    Added argument

--- gtts-pipe.py
+++ gtts-pipe.py
@@ -1,5 +1,12 @@
 import sys
 from gtts import gTTS
 from tempfile import TemporaryFile
-tts = gTTS(text='Hello', lang='en')
+
+if len(sys.argv) < 2:
+    print("Requires an argument to output")
+    exit(1)
+
+tts = gTTS(text=sys.argv[1], lang='en')
 tts.write_to_fp(sys.stdout.buffer)
+
+
